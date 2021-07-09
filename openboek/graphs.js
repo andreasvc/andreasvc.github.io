3e82114527f65a90841f91eb5b8ae169c1a39915
Andreas van Cranenburgh <andreas@unstable.nl>
2021-07-09 09:26:27 +0200

    fix error with chart

--- openboek/graphs.js
+++ openboek/graphs.js
@@ -4,44 +4,7 @@ function alertFunction() {
 
 
 Chart.defaults.font.size = 18;
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['BookX', 'BookY', 'BookZ', 'BookF', 'BookH', 'BookL'],
-        datasets: [{
-            label: '# of ?',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-var ctx = document.getElementById("radar-chart");
-var myChart = new Chart(ctx, {
+var myChart = new Chart('radar-chart', {
     type: 'radar',
     data: {
       labels: ['Anna Karenina', 'De Ellendigen', ['De reis om de wereld','in tachtig dagen'], 'De uitvreter', 'Dichtertje', 'Eline Vere', 'Max Havelaar', ['Sherlock Holmes en', 'de Agra-schat'], 'Titaantjes'],
